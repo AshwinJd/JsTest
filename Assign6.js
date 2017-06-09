aba9337ee082aab8bec799c3c3ede9b7d18e1bb9
AshwinJd <jaydevashwin7@gmail.com>
2017-06-09 16:40:53 +0530

    New commit

--- Assign6.js
+++ Assign6.js
@@ -1,10 +1,14 @@
-const readline = require('readline');
 const fs = require('fs');
-// var jsonfile = require('jsonfile');
-const rl = readline.createInterface({
-  input: fs.createReadStream('g20.csv'),
-//   output: process.stdout
-});
+const readline = require('readline');
+
+
+try{
+   var rl = readline.createInterface({
+    input: fs.createReadStream('g20.csv'),
+    });
+}catch(err){
+    console.log("Enter a correct file",err);
+}
 var file = "data.json";
 
 extract(rl);
@@ -15,8 +19,6 @@ function extract(rl){
     var temp;
     var dataHeader = [];
 
-
-
     rl.on('line', (line) => {
         c++;
         line = line.replace(/"/g, "");
@@ -25,44 +27,27 @@ function extract(rl){
             st.map(function(item){
                 dataHeader.push(item);
                 
-                //console.log(item);
             });
         }else{
             temp = new Object();
             st.map(function(item,index){
                 if(index!=0){
                     temp[dataHeader[index]] = Number(item);
-                    // console.log(item);
                 }else{
                     temp[dataHeader[index]] = item
-                    // console.log(item);
                 }
-                                     
             });
         data.push(temp);
-
-        //    console.log(temp);
-        // fs.write(fd, temp);
         }
 
 
-    //   console.log(data[0]);
     }).on('close',function(){
-        // var final = {}
-        // data.map(function(item){
-        //   final[obj[index]] = item;  
-        // })
-        // console.log(JSON.stringify(data));
         fs.writeFile('data.json',JSON.stringify(data) , (err) => {
         if (err) throw err;
         console.log('The file has been saved!');
         });
         
     });
-        // rl.close(writeData(data))
-    
-    
-
 }
 
 
